65626fa3dd4dc4f37ed3637e5b6d34ba463bc94c
Devin Parkinson <dparky13@icloud.com>
2018-04-03 12:30:20 -0600

    get details of coins

--- app/controllers/api/coins_controller.rb
+++ app/controllers/api/coins_controller.rb
@@ -4,6 +4,13 @@ class Api::CoinsController < ApplicationController
   BASE_URL = 'https://api.coinmarketcap.com/v1/ticker/'
 
   def index
+    coins = HTTParty.get(BASE_URL)
+    user_coins = current_user.coins
+    user_coins.each do |coin|
+      res_coin = coins.find { |c| c['id'] == coin.cmc_id }
+      coin.update(price: res_coin['price_usd']) if res_coin
+    end
+
     render json: current_user.coins
   end
 
@@ -11,8 +18,12 @@ class Api::CoinsController < ApplicationController
     cmc_id = params[:coin].downcase
     res = HTTParty.get("#{BASE_URL}#{cmc_id}")
     if coin = Coin.create_by_cmc_id(res)
-      watched = WatchedCoin.find_or_create_by( coin_id: coin.id, user_id: current_user.id)
+      watched = WatchedCoin.find_or_create_by(
+        coin_id: coin.id,
+        user_id: current_user.id
+      )
       watched.update(initial_price: coin.price) if watched.initial_price.nil?
+      render json: coin
     else
       render json: { errors: 'Coin Not Found' }, status: 422
     end
@@ -22,8 +33,12 @@ class Api::CoinsController < ApplicationController
     render json: @coin
   end
 
+  # PUT /api/coins/:id
   def update
-    current_user.watched_coins.find_by(coin_id: @coin.id).destroy
+    current_user
+      .watched_coins
+      .find_by(coin_id: @coin.id)
+      .destroy
   end
 
   def destroy
--- app/models/coin.rb
+++ app/models/coin.rb
@@ -1,13 +1,13 @@
 class Coin < ApplicationRecord
-  validates_uniqueness_of :cmc_id, :name, :symbol, { case_sensitive: false }
+  validates_uniqueness_of :cmc_id, :name, :symbol, { case_sensivite: false }
   validates_presence_of :cmc_id, :name, :symbol
-
   has_many :watched_coins, dependent: :destroy
   has_many :users, through: :watched_coins
 
   def self.create_by_cmc_id(res)
-    if /^2\d\d$/ =~ res.code.to_s
+    if  /^2\d\d$/ =~ res.code.to_s
       match = res[0].with_indifferent_access
+
       coin_params = {
         name: match[:name],
         symbol: match[:symbol],
@@ -17,8 +17,10 @@ class Coin < ApplicationRecord
       Coin.find_or_create_by(coin_params) do |coin|
         coin.price = match[:price_usd]
         coin.last_fetched = DateTime.now
+      end
     else
       nil
     end
   end
+
 end
